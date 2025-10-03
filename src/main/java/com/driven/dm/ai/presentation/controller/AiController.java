e6282aead24b52b95b36f857298962a848b8529f
Jess <jcscene@gmail.com>
2025-10-03 21:32:02 +0900

    chore: JWT key 설정 추가

--- src/main/java/com/driven/dm/ai/presentation/controller/AiController.java
+++ src/main/java/com/driven/dm/ai/presentation/controller/AiController.java
@@ -4,6 +4,7 @@ import com.driven.dm.ai.application.service.AiService;
 import com.driven.dm.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
