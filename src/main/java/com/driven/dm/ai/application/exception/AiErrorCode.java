54d1666fc91ed4d8ca564da76c407f9f18db9c1b
Jess <jcscene@gmail.com>
2025-10-05 15:34:31 +0900

    feat: Ai 관련 에러코드 클래스 구현

---
+++ src/main/java/com/driven/dm/ai/application/exception/AiErrorCode.java
@@ -0,0 +1,17 @@
+package com.driven.dm.ai.application.exception;
+
+import com.driven.dm.global.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AiErrorCode implements ErrorCode {
+
+    AI_LOG_NOT_FOUND("AI000", "로그를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+    private final String code;
+    private final String message;
+    private final HttpStatus status;
+}
