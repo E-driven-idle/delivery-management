bbbd5f7a75129febbcfe2601cea108f52452eada
changhui98 <net_0102@naver.com>
2025-10-07 23:17:44 +0900

    Feat : 가게 등록 컨트롤러 추가

--- src/main/java/com/driven/dm/shop/presentation/controller/ShopController.java
+++ src/main/java/com/driven/dm/shop/presentation/controller/ShopController.java
@@ -1,4 +1,33 @@
 package com.driven.dm.shop.presentation.controller;
 
+import com.driven.dm.shop.application.service.ShopService;
+import com.driven.dm.shop.presentation.dto.request.ShopDto;
+import com.driven.dm.shop.presentation.dto.response.ShopResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/shops")
+@RequiredArgsConstructor
 public class ShopController {
+
+    private final ShopService shopService;
+
+    @PostMapping
+    public ResponseEntity<ShopResponseDto> createShop(
+        @AuthenticationPrincipal UserDetails userDetails,
+        @RequestBody ShopDto shopDto){
+
+        ShopResponseDto shopResponseDto = shopService.createShop(userDetails, shopDto);
+
+        return ResponseEntity.ok().body(shopResponseDto);
+    }
+
+
 }
