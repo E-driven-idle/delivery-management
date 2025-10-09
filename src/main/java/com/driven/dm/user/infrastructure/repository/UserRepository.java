af8a005548869ef716477ac0e5a12c973be13b33
sejunO <friendlyoh@gmail.com>
2025-10-09 18:56:24 +0900

    Feature/user search (#13)
    * feat: user 검색 API 추가
    
    * feat: user 검색 기능 추가, JPA Auditing 설정, 예외 처리 포맷 추가
    
    - AuditorAware 등록
    - /users 검색 API 추가
    - 글로벌 예외 처리 포맷 추가
    
    * test: UserService 유저 검색 단위 테스트 추가

--- src/main/java/com/driven/dm/user/infrastructure/repository/UserRepository.java
+++ src/main/java/com/driven/dm/user/infrastructure/repository/UserRepository.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
@@ -16,4 +18,19 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByIdAndStatus(UUID id, UserStatus active);
 
     List<User> findAllByUsernameOrNickname(String username, String nickname);
+
+    @Query(
+        value = "select p_user.* "
+            + "from ("
+            + " select p_user.user_id, p_user.created_at "
+            + " from p_user "
+            + " order by p_user.created_at desc "
+            + " limit :limit offset :offset"
+            + ") t left join p_user on t.user_id = p_user.user_id",
+        nativeQuery = true
+    )
+    List<User> findAll(
+        @Param("offset") Long offset,
+        @Param("limit") Long limit
+    );
 }
