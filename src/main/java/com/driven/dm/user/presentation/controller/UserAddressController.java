cb712016888edde6a805a2841a59e7c974061eda
sejunO <friendlyoh@gmail.com>
2025-10-13 18:39:27 +0900

    Feature/user address read (#27)
    * feat: user address 조회
    
    * test: user address 조회 테스트 추가

--- src/main/java/com/driven/dm/user/presentation/controller/UserAddressController.java
+++ src/main/java/com/driven/dm/user/presentation/controller/UserAddressController.java
@@ -4,11 +4,14 @@ import com.driven.dm.global.config.security.SecurityUser;
 import com.driven.dm.user.application.service.UserAddressService;
 import com.driven.dm.user.presentation.dto.request.UserAddressCreateRequest;
 import com.driven.dm.user.presentation.dto.response.UserAddressCreateResponse;
+import com.driven.dm.user.presentation.dto.response.UserAddressResponse;
 import jakarta.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,7 @@ public class
 UserAddressController {
 
     private final UserAddressService userAddressService;
+
     @PostMapping("/api/v1/users/me/addresses")
     public ResponseEntity<UserAddressCreateResponse> createUserAddress(
         @RequestBody @Valid UserAddressCreateRequest request,
@@ -26,5 +30,13 @@ UserAddressController {
     ) {
         UUID userAddressId = userAddressService.createUserAddress(securityUser.getId(), request);
         return ResponseEntity.ok().body(UserAddressCreateResponse.of(userAddressId));
+    }
+
+    @GetMapping("/api/v1/users/me/addresses")
+    public ResponseEntity<List<UserAddressResponse>> getAddresses(
+        @AuthenticationPrincipal SecurityUser securityUser
+    ) {
+        List<UserAddressResponse> response = userAddressService.getAddresses(securityUser.getId());
+        return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/driven/dm/user/presentation/dto/response/UserAddressResponse.java
+++ src/main/java/com/driven/dm/user/presentation/dto/response/UserAddressResponse.java
@@ -1,5 +1,25 @@
 package com.driven.dm.user.presentation.dto.response;
 
-public record UserAddressResponse() {
+import com.driven.dm.user.domain.entity.UserAddress;
+import java.util.UUID;
+import lombok.Builder;
 
+@Builder
+public record UserAddressResponse(
+    UUID id,
+    String zipCode,
+    String primaryAddress,
+    String detailAddress,
+    boolean isDefault
+) {
+
+    public static UserAddressResponse from(UserAddress userAddress) {
+        return UserAddressResponse.builder()
+            .id(userAddress.getId())
+            .zipCode(userAddress.getAddress().getZipCode())
+            .primaryAddress(userAddress.getAddress().getPrimaryAddress())
+            .detailAddress(userAddress.getAddress().getDetailAddress())
+            .isDefault(userAddress.isDefault())
+            .build();
+    }
 }
