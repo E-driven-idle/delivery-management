2b68c776c4a82eb459f94805006d2c4a88c00963
wnsgud0310 <ffjjo0310@naver.com>
2025-10-14 23:16:05 +0900

    feat: 리뷰 및 리뷰 이미지 Repository 구현

--- src/main/java/com/driven/dm/review/infrastructure/repository/ReviewImageRepository.java
+++ src/main/java/com/driven/dm/review/infrastructure/repository/ReviewImageRepository.java
@@ -1,5 +1,11 @@
 package com.driven.dm.review.infrastructure.repository;
 
-public interface ReviewImageRepository {
+import com.driven.dm.review.domain.entity.ReviewImage;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface ReviewImageRepository extends JpaRepository<ReviewImage, UUID> {
+
+    List<ReviewImage> findByReview_IdAndDeletedAtIsNull(UUID reviewId);
 }
