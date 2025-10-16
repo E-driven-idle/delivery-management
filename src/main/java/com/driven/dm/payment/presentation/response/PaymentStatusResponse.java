16ec6c072b622ba156a6bebc29fafb1e4daa63e9
천세경 <jay45652@gmail.com>
2025-10-16 10:13:13 +0900

    feature: 결제 정보 단일 조회 기능

--- src/main/java/com/driven/dm/payment/presentation/response/PaymentStatusResponse.java
+++ src/main/java/com/driven/dm/payment/presentation/response/PaymentStatusResponse.java
@@ -2,6 +2,8 @@ package com.driven.dm.payment.presentation.response;
 
 import java.util.UUID;
 
+import com.driven.dm.payment.domain.entity.Payment;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,4 +13,17 @@ public class PaymentStatusResponse {
 	private UUID paymentId;
 	private String status;
 	private String failureReason;
+
+	public static PaymentStatusResponse from(Payment payment) {
+		if (payment == null) {
+			throw new NullPointerException("결제 정보가 null입니다.");
+		}
+
+		return PaymentStatusResponse.builder()
+									.paymentId(payment.getId())
+									.status(payment.getStatus() != null ? payment.getStatus().name() : null)
+									.failureReason(payment.getFailureReason()) // null 허용 (REJECTED 아닐 경우)
+									.build();
+	}
+
 }
